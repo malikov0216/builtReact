9ccf70ac247e61cdf7bbf0974fc36998a762893a
Нартай Дембаев <mastaok02@gmail.com>
2019-06-05 02:31:46 +0600

    -i commit

---
+++ build/precache-manifest.52cc9dc5d5b3507a22f7cc86ae01f1bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70eea9c8ec7597d9e4ce802553b160cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2d49d71ed38ab452d73",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d2d49d71ed38ab452d73",
+    "url": "/static/js/2.99830a1c.chunk.js"
+  },
+  {
+    "revision": "3959d59c2a8e61f9720c",
+    "url": "/static/js/main.8bdd93c6.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
